975d967e0ffc338cb74ea65a943167729680c398
Kennerus <kennerus@gmail.com>
2017-11-29 14:57:15 +0300

    осталась форма

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// header big ball
 window.onload = function() {
   // Animate
   var anim = function(x,y,maxX,maxY,el) {
@@ -15,11 +15,90 @@ window.onload = function() {
     areaWidth: 200,
     areaHeight: 800,
     calcAngle: false,
-    duration: 20000
+    duration: 30000
   }, function(x,y,angle) {
     bird.style.transform = "translateX("+x+"px) translateY("+y+"px) rotateZ("+(angle)+"deg)";
   });
 
   anim(0,0,0,1, bird);
+
+  var ballR02 = document.getElementById('ballR02'); 
+  flybezier({
+    path: "M129.391,130.778c14.768,3.404,41.872,17.266,46.836-13.813s3.885-41.224,45.757-43.814 s89.787-21.583,5.18-22.447s-105.327,5.396-124.752-13.382S70.9,20.919,51.691,22.43s-14.245-2.158-22.015-8.202 s-42.087-30.865-23.958,6.259s2.59,66.909,49.642,53.743S60.108,114.806,129.391,130.778z",
+    areaWidth: 350,
+    areaHeight: 300,
+    calcAngle: false,
+    duration: 20000
+  }, function(x,y,angle) {
+    ballR02.style.transform = "translateX("+x+"px) translateY("+y+"px) rotateZ("+(angle)+"deg)";
+  });
+  anim(0,0,0,1, ballR02);
 }
 
+// header small ball
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+  var field = document.querySelector('.header');
+  var ball = document.getElementById("ball2");
+
+  var maxXsin = field.clientWidth - ball.offsetWidth;
+  var maxYsin = field.clientHeight - ball.offsetHeight;
+
+  var duration = 60; // seconds
+  var gridSize = 20; // pixels
+
+  var start = null;
+
+  function step(timestamp)
+  {
+    var progress, x, y, y2;
+    if(start === null) start = timestamp;
+
+    progress = (timestamp - start) / duration / 1000; // percent
+
+    x = progress * maxXsin/gridSize; // x = ƒ(t)
+    y = 2 * Math.sin(x); // y = ƒ(x)
+    y2 = 2 * Math.cos(x);
+
+    ball.style.left = Math.min(maxXsin, gridSize * x) + "px";
+    ball.style.bottom = maxYsin - 40 + (gridSize * y) + "px";
+
+    if(progress >= 1) start = null; // reset to start position
+    requestAnimationFrame(step);
+  }
+    requestAnimationFrame(step);
+
+  // about balls
+  // var fieldR = document.querySelector('.about__figures');
+  // var ballR = document.getElementById("ballR02");
+
+  // var maxXrotate = field.clientWidth - ballR02.offsetWidth;
+  // var maxYrotate= field.clientHeight - ballR02.offsetHeight;
+
+  // var durationR = 10; // seconds
+  // var gridSizeR = 50; // pixels
+
+  // var startR = null;
+  // var stretchFactorR;
+
+  // function stepR(timestamp)
+  // {
+  //   var progressR, xR, yR;
+  //   if(startR === null) {
+  //     startR = timestamp;
+  //     stretchFactorR = 1 + (Math.random() * 3);
+  //   }
+
+  //   progressR = (timestamp - startR) / durationR / 1000; // percent
+
+  //   xR = stretchFactorR * Math.sin(progressR * 2 * Math.PI); // x = ƒ(t)
+  //   yR = Math.cos(progressR * 2 * Math.PI); // y = ƒ(t)
+
+  //   ballR02.style.left = maxXrotate/14 + (gridSizeR * xR) + "px";
+  //   ballR02.style.bottom = maxYrotate/4 + (gridSizeR * yR) + "px";
+
+  //   if(progressR >= 1) startR = null; // reset to start position
+  //   requestAnimationFrame(stepR);
+  // }
+
+  // requestAnimationFrame(stepR);
